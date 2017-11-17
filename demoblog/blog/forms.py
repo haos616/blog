1c68e1929e9e5271e2036a3c64f5558f6281d509
Denis Podlesniy <haos616@gmail.com>
2017-11-17 22:11:49 +0200

    Add form

--- demoblog/blog/forms.py
+++ demoblog/blog/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+from django.utils import timezone
 
 from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    is_publish = forms.BooleanField(initial=False, required=False)
+
+    def save(self, user, commit=True):
+        self.instance.author = user
+        if self.cleaned_data['is_publish']:
+            self.instance.published_date = timezone.now()
+        else:
+            self.instance.published_date = None
+        return super(PostForm, self).save(commit)
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', 'is_publish')
